0776ff432099a24c8c29d15594fccf10d985bea5
wodim <neikokz@gmail.com>
2018-09-23 22:00:23 +0200

    disconnect from discord if I've failed to read the squares a few times
    that usually indicates that the user is seeing the login screen

--- script/wow.py
+++ script/wow.py
@@ -7,18 +7,18 @@ import win32con
 import win32gui
 
 
+# variables that can be configured
 DEBUG = 0
-
 MY_WIDTH = 11.61
-
 DISCORD_CLIENT_ID = '483214843734654987'
 
+# these are internal use variables, don't touch them
 decoded = ''
 wow_hwnd = None
 rpc_obj = None
-
 last_first_line = None
 last_second_line = None
+failed_reads = 0
 
 
 def callback(hwnd, extra):
@@ -89,6 +89,8 @@ while True:
     win32gui.EnumWindows(callback, None)
 
     if DEBUG:
+        # if in debug mode, squares are read, the image with the dot matrix is
+        # shown and then the script quits.
         if wow_hwnd:
             print('Debug: reading squares. Is everything alright?')
             read_squares(wow_hwnd)
@@ -96,32 +98,50 @@ while True:
             print("Launching in debug mode but I couldn't find WoW.")
         break
     elif win32gui.GetForegroundWindow() == wow_hwnd:
-        if not rpc_obj:
-            print('Not connected to Discord, connecting...')
-            while True:
-                try:
-                    rpc_obj = (rpc.DiscordIpcClient
-                               .for_platform(DISCORD_CLIENT_ID))
-                except Exception as exc:
-                    print("I couldn't connect to Discord (%s). It's probably "
-                          'not running. I will try again in 5 sec.' % str(exc))
-                    time.sleep(5)
-                    pass
-                else:
-                    break
-            print('Connected to Discord.')
-
         lines = read_squares(wow_hwnd)
 
         if not lines:
+            # there's been a problem reading the squares. if this has happened
+            # too many times, it's probably because the game is in the log in
+            # or character selection screen, so disconnect from discord
+            if failed_reads > 9 and rpc_obj:
+                print("I've failed to read the squares too many times. I will "
+                      "keep trying but by now I'll disconnect from Discord.")
+                rpc_obj.close()
+                rpc_obj = None
+                last_first_line, last_second_line = None, None
+            elif failed_reads > 9:
+                pass
+            else:
+                # bump the number of failed reads
+                failed_reads += 1
+                print('Failed to read the squares: attempt #%d' % failed_reads)
             time.sleep(1)
             continue
 
+        failed_reads = 0
         first_line, second_line = lines
 
         if first_line != last_first_line or second_line != last_second_line:
+            # there has been an update, so send it to discord
             last_first_line = first_line
             last_second_line = second_line
+
+            if not rpc_obj:
+                print('Not connected to Discord, connecting...')
+                while True:
+                    try:
+                        rpc_obj = (rpc.DiscordIpcClient
+                                   .for_platform(DISCORD_CLIENT_ID))
+                    except Exception as exc:
+                        print("I couldn't connect to Discord (%s). It's "
+                              'probably not running. I will try again in 5 '
+                              'sec.' % str(exc))
+                        time.sleep(5)
+                        pass
+                    else:
+                        break
+                print('Connected to Discord.')
 
             print('Setting new activity: %s - %s' % (first_line, second_line))
             activity = {
